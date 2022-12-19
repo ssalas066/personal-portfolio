4102468dcf8685839bdb796e2c92e28551dae6db
Sergio Salas-Villalobos <ssalas28@cnm.edu>
2022-12-19 16:42:33 -0700

    added li links to nav for page navigation

--- portfolio-next/dev-portfolio-nextjs/components/Navbar.jsx
+++ portfolio-next/dev-portfolio-nextjs/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
     return (
@@ -7,6 +8,25 @@ const Navbar = () => {
             <div className='fixed w-full h-20 shadow-xl z-[100]'>
                 <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
                     <Image src='/../public/assets/placeholder-banner.png' alt='/' width='125' height='50'/>
+                    <div>
+                        <ul>
+                            <Link href='/'>
+                                <li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
+                            </Link>
+                            <Link href='/'>
+                                <li className='ml-10 text-sm uppercase hover:border-b'>About</li>
+                            </Link>
+                            <Link href='/'>
+                                <li className='ml-10 text-sm uppercase hover:border-b'>Skills</li>
+                            </Link>
+                            <Link href='/'>
+                                <li className='ml-10 text-sm uppercase hover:border-b'>Projects</li>
+                            </Link>
+                            <Link href='/'>
+                                <li className='ml-10 text-sm uppercase hover:border-b'>Contact</li>
+                            </Link>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
