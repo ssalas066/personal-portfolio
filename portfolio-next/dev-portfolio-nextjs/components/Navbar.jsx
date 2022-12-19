ef602a3aa4dfdece7d321335d8b813b292993769
Sergio Salas-Villalobos <ssalas28@cnm.edu>
2022-12-19 16:36:18 -0700

    working on displaying banner logo for navbar, returning invalid image type

--- portfolio-next/dev-portfolio-nextjs/components/Navbar.jsx
+++ portfolio-next/dev-portfolio-nextjs/components/Navbar.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import Image from "next/image";
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div>
+            <div className='fixed w-full h-20 shadow-xl z-[100]'>
+                <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
+                    <Image src='/./placeholder-banner.png' alt='/' width='125' height='50'/>
+                </div>
+            </div>
+        </div>
     )
 }
 
