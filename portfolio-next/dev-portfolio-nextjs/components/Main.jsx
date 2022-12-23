7836e50380bc9063ad175e0a252addc9780ba3f8
Sergio Salas-Villalobos <ssalas28@cnm.edu>
2022-12-23 00:03:18 -0700

    added text to main component, continuing to work on navbar

--- portfolio-next/dev-portfolio-nextjs/components/Main.jsx
+++ portfolio-next/dev-portfolio-nextjs/components/Main.jsx
@@ -5,10 +5,17 @@ const Main = () => {
         <div className='w-full h-screen text-center'>
             <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
                 <div>
-                    <h1 className='uppercase text-xl tracking-widest text-yellow-300'>Hi, I'm Serge</h1>
-                    <h3>
-                        A Fullstack Web Developer
-                    </h3>
+                    <p className='uppercase text-sm tracking-widest text-gray-600'>
+                        Bring your digital vision to life - one line of code at a time
+                    </p>
+                    <h1 className='py-4 text-gray-700'>
+                        Hi, I'm <span className='text-yellow-500'>Serge</span>
+                    </h1>
+                    <h1 className='py-2 text-gray-700'>Fullstack Web Developer</h1>
+                    <p className='py-4 text-gray-600 max-w-[70%] m-auto'>
+                        I'm a fullstack web developer with experience in application layers, presentation layers, and databases.
+                        I'm currently focused on building responsive and mobile-first front-end web applications, continuing to improve on back-end technologies.
+                    </p>
                 </div>
             </div>
         </div>
